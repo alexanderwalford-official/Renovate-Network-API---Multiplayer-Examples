fb179d5f8479cbc93f628245171b5ace28c80a07
Alexander <74402359+alexanderwalford-official@users.noreply.github.com>
2022-01-10 02:02:55 +0000

    Update RenovateNetworkMultiplayer.cs
    Made it easier to use the class file due to recent changes in the Renovate Network API.

--- C#/RenovateNetworkMultiplayer.cs
+++ C#/RenovateNetworkMultiplayer.cs
@@ -1,89 +1,88 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 // RenovateNetworkMultiplayer.cs
-// Renovate Software LTD 2021
+// Renovate Software LTD 2022
 // Written by Alexander Walford
 
-class RenovateNetwork
+public class RenovateNetworkMultiplayer
 {
-	private static readonly HttpClient client = new HttpClient(); // the HTTP client object
-	
+    private static readonly HttpClient client = new HttpClient(); // the HTTP client object
     public async void SetPlayer(string apikey, string username, string lobby, string position, string rotation)
     {
-		// in this method we're sending the player's data to the server
-		var values = new Dictionary<string, string>
+        // in this method we're sending the player's data to the server
+        var values = new Dictionary<string, string>
         {
             { "apikey", apikey },
             { "playerid", username },
-			{ "lobbyid", lobby },
-			{ "position", position },
-			{ "rotation", rotation }
-			
+            { "lobbyid", lobby },
+            { "position", position },
+            { "rotation", rotation }
+
         };
         var content = new FormUrlEncodedContent(values);
         var response = await client.PostAsync("https://renovatesoftware.com/API/setplayerdata/ ", content);
         var responseString = await response.Content.ReadAsStringAsync();
-        return responseString;
-	}
-	
-	public async void SetGameObject(string apikey, string objectid, string lobby, string sparams)
+    }
+
+    public async void SetGameObject(string apikey, string objectid, string lobby, string sparams)
     {
-		// in this method we're sending game object data to the server
-		var values = new Dictionary<string, string>
+        // in this method we're sending game object data to the server
+        var values = new Dictionary<string, string>
         {
             { "apikey", apikey },
             { "objectid", objectid },
-			{ "lobbyid", lobby },
-			{ "params", sparams },		
+            { "lobbyid", lobby },
+            { "params", sparams },
         };
         var content = new FormUrlEncodedContent(values);
         var response = await client.PostAsync("https://renovatesoftware.com/API/setgameobjectdata/ ", content);
         var responseString = await response.Content.ReadAsStringAsync();
-        return responseString;
-	}
-	
-	public async void GetGameObject(string apikey, string objectid, string lobby)
+    }
+
+    static public string GetPlayerList(string apikey, string lobbyid)
     {
-		// in this method we're sending game object data to the server
-		var values = new Dictionary<string, string>
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://renovatesoftware.com/API/GET/getplayers/" + apikey + "/" + lobbyid + "/");
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
         {
-            { "apikey", apikey },
-            { "objectid", objectid },
-			{ "lobbyid", lobby },		
-        };
-        var content = new FormUrlEncodedContent(values);
-        var response = await client.PostAsync("https://renovatesoftware.com/API/getgameobjectdata/ ", content);
-        var responseString = await response.Content.ReadAsStringAsync();
-        return responseString;
-	}
-	
-	public async void GetPlayer(string apikey, string username)
+            string data = reader.ReadToEnd();
+            return data;
+        }
+    }
+
+    static public string GetPlayerData(string apikey, string playerID)
     {
-		// in this method we're getting the player's data from the server
-		var values = new Dictionary<string, string>
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://renovatesoftware.com/API/GET/getplayerdata/" + apikey + "/" + playerID + "/");
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
         {
-            { "apikey", apikey },
-            { "playerid", username }
-        };
-        var content = new FormUrlEncodedContent(values);
-        var response = await client.PostAsync("https://renovatesoftware.com/API/getplayerdata/ ", content);
-        var responseString = await response.Content.ReadAsStringAsync();
-        return responseString;
-	}		
-	
-	public async void GetPlayerList(string apikey, string lobby)
+            string data = reader.ReadToEnd();
+            return data;
+        }
+    }
+
+    static public string GetGameObject(string apikey, string lobbyid, string objectid)
     {
-		// in this method we're getting the current lobby's player list
-		var values = new Dictionary<string, string>
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://renovatesoftware.com/API/GET/getgameobjectdata/" + apikey + "/" + lobbyid + "/" + objectid + "/");
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
         {
-            { "apikey", apikey },
-            { "lobbyid", lobby }
-        };
-        var content = new FormUrlEncodedContent(values);
-        var response = await client.PostAsync("https://renovatesoftware.com/API/getplayerlist/ ", content);
-        var responseString = await response.Content.ReadAsStringAsync();
-        return responseString;
-	}	
+            string data = reader.ReadToEnd();
+            return data;
+        }
+    }
 }
